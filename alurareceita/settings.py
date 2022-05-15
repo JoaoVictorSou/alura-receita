f5a7ac6f5eed829ef879fbf7ebb097a06e009886
JoaoVictorSou <soulima.joao@gmail.com>
2022-05-15 05:50:40 -0300

    comentários reformulados

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Onde ficarão os arquivos estáticos
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Caminho absoluto para os arquivos estáticos
+STATIC_URL = 'static/' # Referência para arquivos localizados no Root
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "alurareceita/static") # Diretório dos arquivos estáticos
+    os.path.join(BASE_DIR, "alurareceita/static") # Diretórios adicionais para os arquivos estáticos. É pecorrido caso seja optado.
 ]
 
 # Default primary key field type
