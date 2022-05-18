db20abc8169abbd650e064970b5f605c1f094026
JoaoVictorSou <soulima.joao@gmail.com>
2022-05-17 23:53:36 -0300

    Model Receita

--- receitas/views.py
+++ receitas/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Receita
 
 # Create your views here.
 
 def index(request):
-    receitas = {
-        1:'Lasanha',
-        2:'Sopa de legumes',
-        3:'Sorvete',
-        4: 'bolo de chocolate'
+    receitas = Receita.objects.all()
+
+    dados = {
+        'receitas': receitas
     }
 
     return render(request, "index.html", {"nomes_das_receitas": receitas}) # deve-se enviar a requisição juntamente com o arquivo
