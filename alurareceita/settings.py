5990061cf778d7e78eba954a6c3cc994cd7bc9fe
JoaoVictorSou <soulima.joao@gmail.com>
2022-05-17 22:04:50 -0300

    conexão com o postgre

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': '552210',
+        'HOST': '127.0.0.1'
     }
 }
 
--- receitas/views.py
+++ receitas/views.py
@@ -4,7 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html") # deve-se enviar a requisição juntamente com o arquivo
+    receitas = {
+        1:'Lasanha',
+        2:'Sopa de legumes',
+        3:'Sorvete',
+        4: 'bolo de chocolate'
+    }
+
+    return render(request, "index.html", {"nomes_das_receitas": receitas}) # deve-se enviar a requisição juntamente com o arquivo
 
 def receita(request):
     return render(request, "receita.html")
