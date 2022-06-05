f4c91ce2b849e3f2e46cbabe0b9ebb2248193048
JoaoVictorSou <soulima.joao@gmail.com>
2022-06-04 23:17:21 -0300

    organização de partials

--- receitas/models.py
+++ receitas/models.py
@@ -15,3 +15,6 @@ class Receita(models.Model):
     foto_receita = models.ImageField(upload_to = "fotos/%d/%m/%Y/", blank = True)
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     postar = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome_receita
