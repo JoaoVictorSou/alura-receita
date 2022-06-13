5f5e9dc14bf2a20b7a4be5caf31f167fb61f686f
JoaoVictorSou <soulima.joao@gmail.com>
2022-06-13 16:22:50 -0300

    logout de usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -74,7 +74,8 @@ def login(request):
     return render(request, 'usuarios/login.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard(request):
     return render(request, 'usuarios/dashboard.html')
